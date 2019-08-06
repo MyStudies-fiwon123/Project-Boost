202b87e2d4443e275909a8b93dace25590433400
fiwon123 <felipeinoue@yahoo.com.br>
2019-08-06 15:37:45 -0300

    Improve code

--- Project-Boost/Assets/Rocket.cs
+++ Project-Boost/Assets/Rocket.cs
@@ -1,39 +1,99 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
 
     Rigidbody rb;
+    AudioSource audioSource;
+    
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        if (state == State.Alive)
+        {
+            Rotate();
+            Thrust();
+        }
     }
 
-    void ProcessInput()
+    private void Rotate()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rb.AddRelativeForce(Vector3.up);
-        }
+        rb.freezeRotation = true; // take manual control of rotation
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            float rotationThisFrame =  rcsThrust * Time.deltaTime;
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.back);
+            float rotationThisFrame = rcsThrust * Time.deltaTime;
+            transform.Rotate(Vector3.back * rotationThisFrame);
         }
+
+        rb.freezeRotation = true; // resume physics control of rotation
+    }
+
+    private void Thrust()
+    {
+ 
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (state != State.Alive) { return; }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextScene", 1f); // parameterise time
+                break;
+            default:
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1f);
+                break;
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 }
